3e6f33d98b18e276f538162a83f64f1fa85dec69
carlos henrique <carlos.andrade.henrique1@outlook.com>
2025-07-07 20:41:04 -0300

    change: adicionando novo host

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   });
   app.use(cookieParser());
   app.useGlobalFilters(new ZodExceptionFilter());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
